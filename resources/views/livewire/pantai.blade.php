d6d62fa449c5d51bbfc8c0b6d0be0ba07b513d1e
tugaspstiunud <72388016+GaluhGita20@users.noreply.github.com>
2021-12-23 19:51:06 +0800

    asda

--- resources/views/livewire/pantai.blade.php
+++ resources/views/livewire/pantai.blade.php
@@ -11,20 +11,23 @@
         <h3><?php echo $pantai->title ?></h3>
         <div class="col-md-6 drag-this-left">
           <!-- <p class="pretitle"></p> -->
-          <p style="text-align:justify;"><?php echo $pantai->desc_left ?></p>
+          <p style="text-align:justify;" id="desc"><?php echo $pantai->desc_left ?></p>
           <div class="portfolio-widget-1-tabs">
             <ul style="list-style:none">
-              <li><a class="active" data-filter="*" href="#">{{$pantai->sub_title}}</a></li>
+              <li><a class="active" data-filter="*" href="#" >{{$pantai->sub_title}}</a>
+              </li>
               @foreach($pantais as $pantaisub)
               @if($pantaisub->status_tingkatan == "sub")
-              <li><a data-filter=".portfolio-{{$pantaisub->filter}}" href="#">{{$pantaisub->sub_title}}</a></li>
+              <li><a data-filter=".portfolio-{{$pantaisub->filter}}" href="#" id="click-pantai-1">{{$pantaisub->sub_title}}</a>
+              <input type="text" id="desc_pantai_input" value="halo" hidden>
+              <input type="text" id="text-right1" value=" " hidden>
               @endif
               @endforeach
             </ul>
           </div>
         </div>
         <div class="col-md-6 drag-this-right">      
-          <p style="text-align:justify;">
+          <p style="text-align:justify;" id="text-desc-right">
             <?php echo $pantai->desc_right ?>
           </p>
         </div>
