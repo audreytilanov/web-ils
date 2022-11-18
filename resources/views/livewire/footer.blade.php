7cbe72bc4fba7521e3fa8cc469a32f826bbc8327
AUDREY TILANOV <tilanovaudrey@gmail.com>
2022-11-18 18:00:19 +0800

    cp ticketing

--- resources/views/livewire/footer.blade.php
+++ resources/views/livewire/footer.blade.php
@@ -11,7 +11,10 @@
           <br>
           <div class="col-lg-4 col-md-4 col-sm-12">
             <h6 class="text-white col-sm-12">Jl. By Pass i Gusti Ngurah Ray Mumbul Nusa Dua</h6>
-            <h6 class="text-white col-sm-12">Whatsapp : 081237530038 | 081375042155</h6>
+            <h6 class="text-white col-sm-12">Contact Person (WA) : 081237530038 | 081375042155</h6>
+            
+            <h6 class="text-white col-sm-12">Ticketing (WA) : 081237530038</h6>
+
           </div>
           <div class="col-lg-4 col-lg-4 col-sm-12 ms-auto" style="margin-top:auto">
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3943.004203556583!2d115.20020521478487!3d-8.785673293689861!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xf95d67e8b6b0b0d5!2zOMKwNDcnMDguNCJTIDExNcKwMTInMDguNiJF!5e0!3m2!1sid!2sid!4v1668757074684!5m2!1sid!2sid" width="400" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
