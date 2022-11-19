85db074585d63074e99af0c5622fd963729aab5f
AUDREY TILANOV <tilanovaudrey@gmail.com>
2022-11-19 20:20:34 +0800

    button erased

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -10,9 +10,6 @@ $a = 'layouts.template';
   <div class="heroText">
     <h1 class="text-white mt-5 mb-lg-4" data-aos="zoom-in" data-aos-delay="800">Bali Drive Tour<br>We Are Bali Tour Organizer</h1>
     <p class="text-secondary-white-color" style="font-weight:400!important;" data-aos="fade-up" data-aos-delay="1000">Anda butuh perjalanan wisata yang menyenangkan ? anda butuh tiket murah ?<br>anda butuh car rental yang bisa dipercaya ? anda butuh guide yang handal ?<br>temukan semuanya hanya di Bali Drive Tour, Bali Drive Tour oraganiser andalan anda !<br>kami memberikan pelayanan berkualitas dengan cara yang bersahabat dan profesional.</p>
-
-    <br>
-    <a href="{{ route('makanan.view') }}" class="btn btn-warning text-white hover-more" style="border-radius:50px;padding-left:10px!important;padding-right:10px!important;">&nbsp;&nbsp;&nbsp;Cek Paket&nbsp;&nbsp;&nbsp;</a>
   </div>
   <div class="videoWrapper">
     <video autoplay="" loop="" muted="" class="custom-video" poster="banner/banner.png">
